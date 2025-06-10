3e0940f22bdacbc566fff9d037f0abe61fc63b62
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-06-10 00:24:27 -0700

    prototype

---
+++ src/main/kotlin/us/timinc/mc/cobblemon/optionscontim/OptionsConTimMod.kt
@@ -0,0 +1,21 @@
+package us.timinc.mc.cobblemon.optionscontim
+
+import com.cobblemon.mod.common.api.events.CobblemonEvents
+import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.fml.common.Mod
+import us.timinc.mc.cobblemon.optionscontim.config.ConfigBuilder
+import us.timinc.mc.cobblemon.optionscontim.config.OptionsConTimConfig
+
+@Mod(OptionsConTimMod.MOD_ID)
+object OptionsConTimMod {
+    const val MOD_ID = "optionscontim"
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    var config: OptionsConTimConfig = ConfigBuilder.load(OptionsConTimConfig::class.java, MOD_ID)
+
+    init {
+        CobblemonEvents.THROWN_POKEBALL_HIT.subscribe { evt ->
+            if (!evt.pokemon.isBattling && !config.outOfBattleCaptures) evt.cancel()
+        }
+    }
+}
---
+++ src/main/kotlin/us/timinc/mc/cobblemon/optionscontim/config/OptionsConTimConfig.kt
@@ -0,0 +1,5 @@
+package us.timinc.mc.cobblemon.optionscontim.config
+
+class OptionsConTimConfig {
+    val outOfBattleCaptures: Boolean = true
+}
